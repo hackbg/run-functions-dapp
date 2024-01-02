02eca8e8cefc757bbed9029c6e4ae4c21cc5948a
imollov <ivomollov@gmail.com>
2024-01-02 10:41:39 +0200

    fix: avoid loading default page before redirect

--- app/next.config.js
+++ app/next.config.js
@@ -4,6 +4,15 @@ module.exports = {
   images: {
     domains: ['pbs.twimg.com', 'abs.twimg.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/x',
+        permanent: true,
+      },
+    ]
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
